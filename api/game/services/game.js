f3348af420f0e840fe9dbfbddda18bc65f917add
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-06-01 21:41:17 -0300

    feat: criando dados relacionais de Many to many

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -29,15 +29,44 @@ async function getByName(name, entityName) {
 }
 
 async function create(name, entityName) {
-  const item = await getByName(name, entityName)
-
+  const item = await getByName(name, entityName);
   if (!item) {
     return await strapi.services[entityName].create({
       name,
-      slug: slugify(name, { lower: true })
-    })
+      slug: slugify(name, { lower: true }),
+    });
   }
 }
+
+async function createManyToManyData(products) {
+  const developers = {};
+  const publishers = {};
+  const categories = {};
+  const platforms = {};
+
+  products.forEach((product) => {
+    const { developer, publisher, genres, supportedOperatingSystems } = product;
+
+    genres &&
+      genres.forEach((item) => {
+        categories[item] = true;
+      });
+    supportedOperatingSystems &&
+      supportedOperatingSystems.forEach((item) => {
+        platforms[item] = true;
+      });
+    developers[developer] = true;
+    publishers[publisher] = true;
+  });
+
+  return Promise.all([
+    ...Object.keys(developers).map((name) => create(name, "developer")),
+    ...Object.keys(publishers).map((name) => create(name, "publisher")),
+    ...Object.keys(categories).map((name) => create(name, "category")),
+    ...Object.keys(platforms).map((name) => create(name, "platform")),
+  ]);
+}
+
 
 module.exports = {
   populate: async (params) => {
@@ -47,8 +76,6 @@ module.exports = {
 
     console.log(products[0])
 
-    await create(products[0].publisher, "publisher")
-    await create(products[0].developer, "developer")
-
+    await createManyToManyData([products[2], products[3]])
   }
 };
