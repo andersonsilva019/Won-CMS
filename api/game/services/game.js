9c2d1d0c2fb67a75fc490c81a7d64ffa08ac546c
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-06-01 21:32:30 -0300

    feat: criando dados com services

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -6,6 +6,38 @@
  */
 
 const axios = require('axios')
+const slugify = require('slugify')
+
+async function getGameInfo(slug) {
+  const jsdom = require("jsdom")
+  const { JSDOM } = jsdom
+  const body = await axios.get(`https://www.gog.com/game/${slug}`)
+  const dom = new JSDOM(body.data)
+
+  const description = dom.window.document.querySelector('.description')
+
+  return {
+    rating: 'BR0',
+    short_description: description.textContent.slice(0, 160),
+    description: description.innerHTML
+  }
+}
+
+async function getByName(name, entityName) {
+  const item = await strapi.services[entityName].find({ name })
+  return item.length ? item[0] : null;
+}
+
+async function create(name, entityName) {
+  const item = await getByName(name, entityName)
+
+  if (!item) {
+    return await strapi.services[entityName].create({
+      name,
+      slug: slugify(name, { lower: true })
+    })
+  }
+}
 
 module.exports = {
   populate: async (params) => {
@@ -13,7 +45,10 @@ module.exports = {
 
     const { data: { products } } = await axios.get(gogApiUrl)
 
-    console.log(products)
+    console.log(products[0])
+
+    await create(products[0].publisher, "publisher")
+    await create(products[0].developer, "developer")
 
   }
 };
