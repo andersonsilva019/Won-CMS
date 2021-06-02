35da6065873a2a0821e76992d3068b9f5e5a1d16
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-06-01 21:45:17 -0300

    feat: criando estrutura do game

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -18,7 +18,7 @@ async function getGameInfo(slug) {
 
   return {
     rating: 'BR0',
-    short_description: description.textContent.slice(0, 160),
+    short_description: description.textContent.trim().slice(0, 160),
     description: description.innerHTML
   }
 }
@@ -67,6 +67,39 @@ async function createManyToManyData(products) {
   ]);
 }
 
+async function createGames(products) {
+  await Promise.all(
+    products.map(async (product) => {
+      const item = await getByName(product.title, "game");
+
+      if (!item) {
+        console.info(`Creating: ${product.title}...`);
+
+        const game = await strapi.services.game.create({
+          name: product.title,
+          slug: product.slug.replace(/_/g, "-"),
+          price: product.price.amount,
+          release_date: new Date(
+            Number(product.globalReleaseDate) * 1000
+          ).toISOString(),
+          categories: await Promise.all(
+            product.genres.map((name) => getByName(name, "category"))
+          ),
+          platforms: await Promise.all(
+            product.supportedOperatingSystems.map((name) =>
+              getByName(name, "platform")
+            )
+          ),
+          developers: [await getByName(product.developer, "developer")],
+          publisher: await getByName(product.publisher, "publisher"),
+          ...(await getGameInfo(product.slug)),
+        });
+
+        return game;
+      }
+    })
+  );
+}
 
 module.exports = {
   populate: async (params) => {
@@ -77,5 +110,6 @@ module.exports = {
     console.log(products[0])
 
     await createManyToManyData([products[2], products[3]])
+    await createGames([products[2], products[3]]);
   }
 };
